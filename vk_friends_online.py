89eb50875733d1eca75e68014a4c3d86e8905c88
kaptsov <kaptsovaa@gmail.com>
2018-02-15 15:18:45 +0900

    Первая попытка сдать

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,15 @@
 import vk
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6371981
 
 
 def get_user_login():
-    pass
+    return input('login:')
 
 
 def get_user_password():
-    pass
+    return input('password:')
 
 
 def get_online_friends(login, password):
@@ -17,16 +17,23 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope="friends",
     )
-    api = vk.API(session)
-    # например, api.friends.get()
+    api_context = vk.API(session)
+    friends_online_id_list = api_context.friends.getOnline()
+    return api_context.users.get(user_ids=friends_online_id_list)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Look, who is online:\n')
+    for friend in friends_online:
+        print(friend['last_name'], friend['first_name'])
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        friends_online = get_online_friends(login, password)
+        output_friends_to_console(friends_online)
+    except(vk.exceptions.VkAuthError):
+        exit('Wrong login or password.')
